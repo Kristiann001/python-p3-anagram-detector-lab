78b7039d56e64d9516c8521b58212b9d0f3dfcf7
Kristian <originalkristiann@mail.com>
2024-05-28 11:21:45 +0300

    add anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        matches = []
+        for anagram in possible_anagrams:
+            if self.is_anagram(anagram.lower()):
+                matches.append(anagram)
+        return matches
+
+    def is_anagram(self, candidate):
+        if candidate == self.word:
+            return False  # Exclude the word itself
+        return sorted(candidate) == sorted(self.word)
